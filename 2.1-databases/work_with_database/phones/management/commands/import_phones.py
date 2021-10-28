b2ea01b1e34c379f0d513765ed0f672f5fe78911
lawejr <dev.lawejr@gmail.com>
2021-10-28 23:49:35 +0500

    Реализована команда для сохранения данных и csv

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,6 @@
 import csv
-
 from django.core.management.base import BaseCommand
+from datetime import datetime
 from phones.models import Phone
 
 
@@ -13,5 +13,11 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.update_or_create(
+                id=phone['id'],
+                name=phone['name'],
+                image=phone['image'],
+                price=float(phone['price']),
+                release_date=datetime.strptime(phone['release_date'], "%Y-%m-%d"),
+                lte_exists=bool(phone['lte_exists'])
+            )
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.TextField(primary_key=True)
+    name = models.TextField()
+    price = models.FloatField()
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = AutoSlugField(populate_from='name')
