f24f8dcf60e1cb11655629a453d1d1423edec726
lawejr <dev.lawejr@gmail.com>
2021-10-29 10:59:00 +0500

    Реализовано отображение страниц каталога и конкретного товара

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +9,35 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    phones = Phone.objects.all()
+
+    if sort:
+        sort = sort.lower()
+        if sort == 'name':
+            phones = phones.order_by('name')
+        elif sort == 'min_price':
+            phones = phones.order_by('price')
+        elif sort == 'max_price':
+            phones = phones.order_by('-price')
+
+    context = {
+        'phones': phones
+    }
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    try:
+        phone = Phone.objects.get(slug=slug)
+    except Phone.DoesNotExist:
+        return HttpResponse('Страница не найдена')
+
+    context = {
+        'phone': phone
+    }
+
     return render(request, template, context)
