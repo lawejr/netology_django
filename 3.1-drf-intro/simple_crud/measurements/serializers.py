a85582716e372e9194931dd8f54747af7621a26c
lawejr <dev.lawejr@gmail.com>
2022-03-15 22:21:52 +0500

    3.1 Выполнено задание по основам DRF

--- 3.1-drf-intro/simple_crud/measurements/serializers.py
+++ 3.1-drf-intro/simple_crud/measurements/serializers.py
@@ -1,1 +1,15 @@
-# TODO: опишите сериализаторы
+from rest_framework.serializers import ModelSerializer
+
+from .models import Project, Measurement
+
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+
+class MeasurementSerializer(ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = "__all__"
