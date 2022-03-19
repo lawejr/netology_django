d6aa7b455b6c2caaa5a20a3f792c01e3d175e645
lawejr <dev.lawejr@gmail.com>
2022-03-19 13:43:39 +0500

    3.2 Реализована возможность поиска продуктов по названию/описанию

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Product, Stock
 from .serializers import ProductSerializer, StockSerializer
@@ -7,7 +9,8 @@ from .serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
