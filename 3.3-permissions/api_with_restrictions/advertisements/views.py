498fdf094061e11cdb05088faa73185caad09ea7
lawejr <dev.lawejr@gmail.com>
2022-03-22 18:36:43 +0500

    3.3 реализована отдача и фильтрация списка объявлений по query

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,12 +1,19 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
 
     def get_permissions(self):
         """Получение прав для действий."""
