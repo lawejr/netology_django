bf8342367c019af694c72e3c5d8000ce8351d239
lawejr <dev.lawejr@gmail.com>
2022-03-26 17:28:12 +0500

    3.3 добавлены ограничения на количество запросов от пользователей

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -76,7 +76,15 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/day',
+        'user': '20/day'
+    }
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
