49b86cba432466cf7a89bcb34c3c476fe2bae651
lawejr <dev.lawejr@gmail.com>
2022-05-20 15:39:45 +0500

    test filter courses by id was added

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -21,8 +21,22 @@ def test_get_first_course(client, course_factory):
 def test_get_some_courses(client, course_factory):
     quantity = 12
     url = reverse('courses-list')
+    course_factory(_quantity=quantity)
     resp = client.get(url)
     assert resp.status_code == HTTP_200_OK
     results = resp.json()
     assert results
     assert len(results) == quantity
+
+
+@pytest.mark.django_db
+def test_id_filter_courses(client, course_factory):
+    quantity = 12
+    courses = course_factory(_quantity=quantity)
+    filter_id = courses[0].id
+    url = reverse('courses-list') + f'?id={filter_id}'
+    resp = client.get(url)
+    assert resp.status_code == HTTP_200_OK
+    results = resp.json()
+    assert results
+    assert courses[0].id == results[0]['id']
